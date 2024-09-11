28c137c692c8b4e5e464f64657633df7e683f700
Uyaii <maryanneinyang@gmail.com>
2024-09-11 22:15:45 +0100

    added typewritter effect

--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -1,13 +1,32 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Typed from "typed.js";
+import "../css/landingPage.css";
 
 const LandingPage = () => {
+  const typingEffectElement = useRef(null);
+  useEffect(() => {
+    const typed = new Typed(typingEffectElement.current, {
+      strings: [
+        "Welcome To Todoly!!!",
+        "A Place To Boost Your Productivity",
+        "Come Join The Fun",
+      ],
+      typeSpeed: 40,
+      backSpeed: 50,
+      loop: true,
+      showCursor:false
+    });
+    return () => {
+      typed.destroy();
+    };
+  }, []);
   return (
-    <div>
-      <h1>Todoly</h1>
+    <div className="landing">
+      <span ref={typingEffectElement} className="typingEffect" />
 
       <p>
         Organize Your Day, Conquer Your Goals: Your Ultimate Productivity Hub
